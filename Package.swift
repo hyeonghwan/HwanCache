00acbf124e8384364afe0923503138d6c22eb988
pHH <gudghks56@gmail.com>
2025-11-25 20:31:41 +0900

    chore: package.swift 생성

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "HwanCache",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15)
+    ],
+    products: [
+        .library(
+            name: "HwanCache",
+            targets: ["HwanCache"]
+        ),
+    ],
+    dependencies: [ ],
+    targets: [
+        .target(
+            name: "HwanCache",
+            dependencies: []
+        ),
+        .testTarget(
+            name: "HwanCacheTests",
+            dependencies: ["HwanCache"]
+        ),
+    ]
+)
